de6af4fb5c16c19dc6591fa2c7c8f43abfaf9703
Piyush <piyush45in@gmail.com>
2025-08-23 09:59:46 +0530

    feature first commit

--- first.js
+++ first.js
@@ -3,3 +3,5 @@ document.getElementById('h1').innerHTML="Learning Git and GitHub";
 const age = 20
 const address = "CL Sharma Building"
 const dist = "Munger"
+const head  = document.querySelector("#heading")
+head.innerText='Learing Git and GitHub';
